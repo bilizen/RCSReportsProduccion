53eeb3a072b6fc543e1c9659140c6da4f33d469e
bilizen <bilizen1@hotmail.com>
2016-10-11 16:44:38 -0500

    PRODUCCION IOS "3.1.12"
    id        = "com.retailcustomsolutions.reports"
    versionCode="27"
    version   ="3.1.12">

--- js/ip.js
+++ js/ip.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     document.addEventListener("deviceready", onDeviceReady, false);
 
     function onDeviceReady() {
-        //document.addEventListener("backbutton", onBackKeyDown, true);
+        document.addEventListener("backbutton", onBackKeyDown, true);
         
         if(checkNetConnection()==true){
             var variablEE = obtenerVariables("variable");
@@ -27,7 +27,6 @@ $(document).ready(function () {
             }
         }
     }
-    
     function onBackKeyDown() {
         navigator.app.exitApp();     
     }
@@ -35,27 +34,27 @@ $(document).ready(function () {
 
 $(window).load(function(){
     // if(checkNetConnection()==true){
-    //         var variablEE = obtenerVariables("variable");
-    //         //-1 si si ingresa por primera vez o mata aplicacion
-    //         onInit();
-    //         deteclenguage();
-    //         if(variablEE == -1){
-    //             existsData();
-    //         }else{   
-    //             //camibar el nombre del boton de ingresar --> agregar
-    //             MSG_GO_ADD();
-    //             $("#btn_left").removeAttr("hidden");
-    //         }
-    //     }else{
-    //         $('#no_connection').modal('show');
-    //         if (current_lang == 'es'){
-    //             $('.titleMessage').text('Mensaje');
-    //             $('.textNoConnection').text('No hay conexion de red');
-    //             $('.btnok').text('Aceptar');
-    //         }else{
-    //            //modal para no conexcion
-    //         }
+    //     var variablEE = obtenerVariables("variable");
+    //     //-1 si si ingresa por primera vez o mata aplicacion
+    //     onInit();
+    //     deteclenguage();
+    //     if(variablEE == -1){
+    //         existsData();
+    //     }else{   
+    //         //camibar el nombre del boton de ingresar --> agregar
+    //         MSG_GO_ADD();
+    //         $("#btn_left").removeAttr("hidden");
     //     }
+    // }else{
+    //     $('#no_connection').modal('show');
+    //     if (current_lang == 'es'){
+    //         $('.titleMessage').text('Mensaje');
+    //         $('.textNoConnection').text('No hay conexion de red');
+    //         $('.btnok').text('Aceptar');
+    //     }else{
+    //        //modal para no conexcion
+    //     }
+    // }
     /*esta funcion ocurre despues de que ya cargo toda la pagina*/
     var lang = navigator.language.split("-");
     var current_lang = (lang[0]);
